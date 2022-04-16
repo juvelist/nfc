866ee4c1edc217fa12c7a87ec2ef8068a047340e
juvelist <39278128+juvelist@users.noreply.github.com>
2022-04-16 13:30:39 +0400

    translations, s=getting, soon

---
+++ js/script.js
@@ -0,0 +1,55 @@
+
+// lang
+$(document).ready(function(){
+  var dictionary, set_lang, en, es;
+
+  // $.getJSON("../locales/en.json", function(json) {
+  //   en = json
+  // });
+  // $.getJSON("../locales/es.json", function(json) {
+  //   es = json
+  // });
+
+  // Object literal behaving as multi-dictionary
+  dictionary = {
+    "en": {
+      "Getting started": "Getting started",
+      "Soon": "Soon",
+    },
+    "es": {
+      "Getting started": "Empezando",
+      "Soon": "Pronto",
+    }
+  };
+
+  // Function for swapping dictionaries
+  set_lang = function (dictionary) {
+    $("[data-translate]").text(function () {
+      var key = $(this).data("translate");
+      if (dictionary.hasOwnProperty(key)) {
+        return dictionary[key];
+      }
+    });
+  };
+
+  //
+  $(document).on("click", function (event) {
+    if ($(event.target).hasClass('lang__selected')) {
+      $("#lang").addClass('active');
+    } else {
+      $("#lang").removeClass('active');
+    }
+  });
+
+  // Swap languages when menu changes
+  $(".lang__item").on("click", function (e) {
+    var language = $(e.target).text().toLowerCase();
+    if (dictionary.hasOwnProperty(language)) {
+      set_lang(dictionary[language]);
+    }
+    $("#lang").removeClass('active');
+  });
+
+  // Set initial language to English
+  set_lang(dictionary.en);
+});
